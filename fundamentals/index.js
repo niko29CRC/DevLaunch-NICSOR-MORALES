f21fce4e9d048a3a029d202638dea3f3c3217eda
Nick Morales <nicsor29@gmail.com>
2025-08-09 14:20:06 -0600

    chore(fundamentals): add input exercise

---
+++ fundamentals/index.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+const name = prompt("What's your name? ")
+
+console.log("Nice day", name, "!!!")
+
+const number = prompt("What's your lucky number: ")
+
+console.log("Your number is:", number)
