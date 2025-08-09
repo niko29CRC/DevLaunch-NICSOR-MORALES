91a12ae16d06c0dd3b714bb6a3c2f1d10556444f
Nick Morales <nicsor29@gmail.com>
2025-08-09 14:27:04 -0600

    chore(fundamentals): add naming convention

--- fundamentals/index.js
+++ fundamentals/index.js
@@ -7,3 +7,16 @@ console.log("Nice day", name, "!!!")
 const number = prompt("What's your lucky number: ")
 
 console.log("Your number is:", number)
+
+// Camel case
+const firstName = "Nicsor"
+const isValidNumber = "yes"
+
+function getRandomNumber() {
+
+  // Cuerpo del correo
+}
+
+// Snake
+const is_countable = "no"
+
