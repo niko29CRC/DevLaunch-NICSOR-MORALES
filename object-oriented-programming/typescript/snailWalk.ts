ef70fdb32f4f88e7b119aaeb44ddec2974db0fcc
Nick Morales <nicsor29@gmail.com>
2025-09-14 13:37:40 -0600

    chore(data-structures): add snailkwalk exercise

--- object-oriented-programming/typescript/snailWalk.ts
+++ object-oriented-programming/typescript/snailWalk.ts
@@ -3,104 +3,101 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
+type Direction = 'RIGHT' | 'LEFT' | 'UP' | 'DOWN';
+const STEP: string = '*';
+const matrixPath: string[][] = [['', '', '',''], ['', '', '',''], ['', '', '',''], ['', '', '','']];
 
-const step: string = '*';
-const matrixPath: string [][] = [['', '', ''], ['', '', ''], ['', '', '']];
 
+function printMatrix(matrix: string[][]): boolean {
+  const space: string = ' '
+  let isCompleted: boolean = true
 
-function printMatrix(matrix:string[][]) : boolean {
-   const space: string = ' '
-   let isCompleted: boolean = true
-    
-   for(let i = 0; i < matrix.length; i++) {
-      let row: string = '';
-      for(let j = 0; j < matrix.length; j++) {
-         if (matrix[i][j] !== step) {
-           isCompleted = false
-         }
-         row += matrix[i][j] || space;
+  for (let i = 0; i < matrix.length; i++) {
+    let row: string = '';
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[i][j] !== STEP) {
+        isCompleted = false
       }
+      row += matrix[i][j] || space;
+    }
 
-      console.log(row);
-   }
-    
-   return isCompleted;
+    console.log(row);
+  }
+
+  return isCompleted;
 }
 
-function snailWalkFiller(matrix:string[][]) {
-   type Direction = 'RIGHT' | 'LEFT' | 'UP' | 'DOWN';
-   let move: Direction
-   let isIncomplete: boolean = false;
+function snailWalkFiller(matrix: string[][]) {
 
-   let i: number = 0;
-   let j: number = 0;
-   move = 'RIGHT'
+  let move: Direction = 'RIGHT'
+  let isIncomplete: boolean = false;
 
-   do {
-           
-      console.log('i=', i, 'j=', j)
+  let i: number = 0;
+  let j: number = 0;
 
-      if (i < matrix.length && j < matrix.length) {
-         if(matrix[i][j] !== step) {
-            matrix[i][j] = step
-         }   
+  do {
+
+    const isInBounds = i < matrix.length && j < matrix.length
+
+    if (isInBounds) {
+      if (matrix[i][j] !== STEP) {
+        matrix[i][j] = STEP
+      }
+    }
+
+    switch (move) {
+      case 'RIGHT': {
+        if ((j + 1) == matrix.length) {
+          move = 'DOWN'
+          i += 1
+        } else {
+          j += 1
+        }
+        break;
+      }
+      case 'DOWN': {
+        if ((i + 1) == matrix.length) {
+          move = 'LEFT'
+          j -= 1
+        } else {
+          i += 1
+        }
+        break;
       }
 
-      switch (move) {
-        case 'RIGHT': {
-         if ( (j + 1) == matrix.length) {
-           move = 'DOWN'
-           i += 1
-         } else {
-           j += 1 
-         }
-         break;
-       } 
-
-        case 'DOWN': {
-          if ( (i + 1) == matrix.length) {
-           move = 'LEFT'
-           j -= 1
-         } else {
-           i += 1 
-         }
-         break;
+      case 'LEFT': {
+        if ((j - 1) == -1) {
+          move = 'UP'
+          i -= 1
+        } else {
+          j -= 1
         }
-
-        case 'LEFT': {
-          if ( (j - 1) == -1) {
-           move = 'UP'
-           i -= 1
-         } else {
-           j -= 1 
-         }
-         break;
-        }
-
-        case 'UP': {
-          if ( (i - 1) == -1) {
-           move = 'RIGHT'
-           j += 1
-         } else {
-           if(matrix[i-1][j] = step) {
-             move = 'RIGHT'
-             j += 1
-           } else {
-             i -= 1 
-           }
-         }
-         break;
-        }
-
-        default: 
-           isIncomplete = true
-           break;
+        break;
       }
 
-      isIncomplete = printMatrix(matrix);
-      const result = prompt(' ');
-   }while (!isIncomplete);
+      case 'UP': {
+        if ((i - 1) == -1) {
+          move = 'RIGHT'
+          j += 1
+        } else {
+          if (matrix[i - 1][j] = STEP) {
+            move = 'RIGHT'
+            j += 1
+          } else {
+            i -= 1
+          }
+        }
+        break;
+      }
 
+      default:
+        isIncomplete = true
+        break;
+    }
+
+    isIncomplete = printMatrix(matrix);
+    const result = prompt(' ');
+  } while (!isIncomplete);
 }
 
 snailWalkFiller(matrixPath);
